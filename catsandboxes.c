3d7ff675e9f937578ec368f45befe7d17dc309a0
Guilherme <guilhermemb11@gmail.com>
2023-05-21 00:12:52 -0300

    first

---
+++ catsandboxes.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "catsandboxes.h"
+
+void oi(void) {
+  printf("oi!\n");
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "catsandboxes.h"
+
+int main(void) {
+
+  printf("hello?\n");
+  oi();
+
+  return 0;
+}
