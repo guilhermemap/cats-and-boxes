604658d21d60961a50e06f86c21636726f2e0bc0
Guilherme <guilherme@acheipneus.com.br>
2023-05-22 18:00:32 -0300

    test.sh com arg pra número de vezes

--- test.sh
+++ test.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
+MAX=10
+if [[ $1 =~ ^[0-9]+$ ]]; 
+then
+   MAX=$1
+fi
 
-for _ in $(seq 10)
-do
-  
+echo "testando $MAX vezes..."
+ERRO=0
+
+for _ in $(seq "$MAX")
+do  
   if ! ./a.out > /dev/null
   then
+    ERRO+=1
     echo "erro..."
   fi
-
 done
+
+if [ $ERRO -gt 0 ]
+then
+  echo "$ERRO erros!"
+else 
+  echo "ok"
+fi
